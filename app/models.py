476ea9a5d3eb7c7b3a8f94b2f1bc647188342d25
Quân Nguyễn <quannh.is55@gmail.com>
2015-08-20 13:45:41 +0700

    Create user profile and user page

--- app/models.py
+++ app/models.py
@@ -6,9 +6,14 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     social_id = db.Column(db.String(64), nullable=False, unique=True)
     nickname = db.Column(db.String(64), nullable=False)
+    avatar_url = db.Column(db.String(254), nullable=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
-    def __repr__(self):
-        return '<User %r>' % self.nickname
+
+def __repr__(self):
+    return '<User %r>' % self.nickname
 
 
 class Post(db.Model):
